4ea41b8865cef73e88489cbad7e0b22ae3fcf47e
Wajahat5 <wajahatyusuf@gmail.com>
2020-07-19 15:49:49 +0530

    4th commit

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ axios.get('/noticedata')
 .then((res)=>{
     console.log(res)
     var i
-    for (i = res.data.length-1; i > res.data.length-6; i--) {
+    for (i = res.data.length-1; (i > res.data.length-6)&&(i>-1); i--) {
     html+=`<a href='notice/${res.data[i]._id}' ><li>`+res.data[i].front+'<a>'
 }
 
